268140aa52229be1a7a21728177f7da872ad3e4a
Leon <304552328@qq.com>
2014-01-12 09:34:37 +0800

    Temp

--- app/controllers/Login.java
+++ app/controllers/Login.java
@@ -1,13 +1,16 @@
 package controllers;
 
-import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
-import play.Application;
-import play.Play;
+import play.Logger;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.Controller;
-import play.mvc.Http.Context;
 import play.mvc.Result;
 import views.html.login;
+import views.html.post;
 
 public class Login extends Controller {
 
@@ -19,6 +22,8 @@ public class Login extends Controller {
 	 * @return
 	 */
     public static Result page(String s, Boolean p, String c) {
+    	String referer = request().getHeader("referer");
+    	Logger.info(referer);
         return ok(login.render(s, p, c));
     }
 
@@ -28,9 +33,36 @@ public class Login extends Controller {
      * @param p password
      * @return
      */
-    public static Result auth(String u, String p, String c) {
+    public static Result auth() {
     	// validate u and p.
-        return redirect("");
+    	DynamicForm form = Form.form().bindFromRequest();
+    	String username = form.get("username");
+    	String password = form.get("password");
+    	
+    	// 验证用户名密码
+    	if(auth(username, password)) {
+    		// 通过
+    	} else {
+    		// 不通过
+    	}
+    	
+    	String c = form.get("continue");
+    	
+    	try {
+    		// TODO
+			java.net.URLEncoder.encode(c, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			Logger.error("", e);
+			// do nothing
+		}
+    	Map<String, String> para = new HashMap<String, String>();
+    	para.put("token", "abcdefg");
+        return ok(post.render(c, para));
+    }
+    
+    public static boolean auth(String username, String password) {
+    	return true;
+    	
     }
 
 }
